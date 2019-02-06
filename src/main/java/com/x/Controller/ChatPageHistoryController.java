1c8b45418fc9cb85505fd3286d1c029cf445887a
Pieczkowski <pieczkowski.t@gmail.com>
2019-02-06 14:12:37 +0100

    Add ChatPageHistoryController

---
+++ src/main/java/com/x/Controller/ChatPageHistoryController.java
@@ -0,0 +1,25 @@
+package com.x.Controller;
+
+import com.x.Service.ArchivedMessageService;
+import com.x.Service.ArchivedMessageServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatPageHistoryController {
+
+    private ArchivedMessageServiceInterface archive;
+
+    @Autowired
+    ChatPageHistoryController(ArchivedMessageService archive){
+        this.archive = archive;
+    }
+
+    @GetMapping("/index")
+    public String sendHistory(Model model){
+        model.addAttribute(archive.getAllArchivedMessages());
+        return "index";
+    }
+}

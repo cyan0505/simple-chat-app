244cc9fd8c65c1b8bd8bfe71917a5e97f8f3eca9
Pieczkowski <pieczkowski.t@gmail.com>
2019-02-07 05:13:00 +0100

    Remove unnecessary things from javaScript file associated with login page

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,8 +1,5 @@
 'use strict';
 
-var usernamePage = document.querySelector('#username-page');
-var chatPage = document.querySelector('#chat-page');
-var usernameForm = document.querySelector('#usernameForm');
 var messageForm = document.querySelector('#messageForm');
 var messageInput = document.querySelector('#message');
 var messageArea = document.querySelector('#messageArea');
@@ -16,19 +13,16 @@ var colors = [
     '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
 ];
 
-function connect(event) {
-    username = document.querySelector('#name').value.trim();
+function connect() {
+    username = document.querySelector('#name').innerText.trim();
 
     if(username) {
-        usernamePage.classList.add('hidden');
-        chatPage.classList.remove('hidden');
 
         var socket = new SockJS('/javatechie');
         stompClient = Stomp.over(socket);
 
         stompClient.connect({}, onConnected, onError);
     }
-    event.preventDefault();
 }
 
 
@@ -117,5 +111,6 @@ function getAvatarColor(messageSender) {
     return colors[index];
 }
 
-usernameForm.addEventListener('submit', connect, true);
 messageForm.addEventListener('submit', send, true);
+
+connect();
